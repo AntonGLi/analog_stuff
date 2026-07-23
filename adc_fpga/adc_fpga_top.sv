dd15c74e98f06686803473cfa83c81087f8890b3
AntonGLi <anthonglinin@yandex.ru>
2026-07-24 02:25:36 +0300

    byte  packer update, verif required

--- adc_fpga/adc_fpga_top.sv
+++ adc_fpga/adc_fpga_top.sv
@@ -46,20 +46,46 @@ sample_processor DSP (
 //byte packer and uart logic
 
 logic [15:0] sample_fifo;
-logic        byte_vld;
+logic [01:0] send_rdy;
+logic [07:0] byte;
+logic        sample_fifo_rdy;
 logic        uart_rdy;
+logic        byte_vld;
+
+
+assign sample_fifo_rdy = (send_rdy[1] == 1'b0);
 
 always_ff @(posedge CLK_50) begin
     if (RST) begin
         sample_fifo <= '0;
-        byte_vld <= '0;
+        send_rdy <= '0;
     end else begin
         if (sample_vld) begin
-            sample_fifo <= {sample_fifo[3:0], sample_processed};
+
+            case (send_rdy)
+                2'b00:
+                    sample_fifo <= {sample_fifo[15:12], sample_processed};
+                2'b01:
+                    sample_fifo <= {sample_processed[3:0], sample_fifo[11:8], sample_processed[7:0]};
+            endcase
+
             byte_vld <= '1;
-        end else if (uart_rdy)
-            byte_vld <= '0;
+            send_rdy <= send_rdy + 2'b01;
+
+        end else if (uart_rdy) begin
+            if (send_rdy==1'b10)
+                send_rdy <= send_rdy + 2'b01;
+            if (send_rdy==1'b11 || send_rdy==1'b00)
+                byte_vld <= 0;
+        end
     end
+end
+
+always_comb begin
+    if (send_rdy[1])
+            byte = sample_fifo[15:8];
+    else
+            byte = sample_fifo[7:0]; 
 end
 
 not_my_uart_tx #(
