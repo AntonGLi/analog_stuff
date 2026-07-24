a10309e4a9e53bfa01fd69fcfb5e827e304c493f
a.glinin <a.glinin@istc-miet.ru>
2026-07-24 17:50:37 +0300

    a

--- adc_fpga/adc_fpga_top.sv
+++ adc_fpga/adc_fpga_top.sv
@@ -47,7 +47,7 @@ sample_processor DSP (
 
 logic [15:0] sample_fifo;
 logic [01:0] send_rdy;
-logic [07:0] byte;
+logic [07:0] byte_packed;
 logic        sample_fifo_rdy;
 logic        uart_rdy;
 logic        byte_vld;
@@ -73,9 +73,9 @@ always_ff @(posedge CLK_50) begin
             send_rdy <= send_rdy + 2'b01;
 
         end else if (uart_rdy) begin
-            if (send_rdy==1'b10)
+            if (send_rdy==2'b10)
                 send_rdy <= send_rdy + 2'b01;
-            if (send_rdy==1'b11 || send_rdy==1'b00)
+            if (send_rdy==2'b11 || send_rdy==2'b00)
                 byte_vld <= 0;
         end
     end
@@ -83,9 +83,9 @@ end
 
 always_comb begin
     if (send_rdy[1])
-            byte = sample_fifo[15:8];
+            byte_packed = sample_fifo[15:8];
     else
-            byte = sample_fifo[7:0]; 
+            byte_packed = sample_fifo[7:0]; 
 end
 
 not_my_uart_tx #(
--- adc_fpga/tb_adc_top_verif_wrap.sv
+++ adc_fpga/tb_adc_top_verif_wrap.sv
@@ -34,44 +34,50 @@ sample_processor DSP (
     .fifo_rdy(1'b1)
 );
 
+
 //byte packer and uart logic
 
 logic [15:0] sample_fifo;
-logic        byte_vld;
-logic        uart_rdy;
-logic        toggle;
+logic [01:0] send_rdy;
+logic [07:0] byte_packed;
 logic        sample_fifo_rdy;
+logic        uart_rdy;
+logic        byte_vld;
+
+
+assign sample_fifo_rdy = (send_rdy[1] == 1'b0);
 
 always_ff @(posedge CLK_50) begin
     if (RST) begin
         sample_fifo <= '0;
-        sample_fifo_ptr <= '0;
+        send_rdy <= '0;
     end else begin
-        if (sample_vld && sample_fifo_rdy) begin
-            sample_fifo <= {sample_fifo[3:0], sample_processed};
+        if (sample_vld) begin
+
+            case (send_rdy)
+                2'b00:
+                    sample_fifo <= {sample_fifo[15:12], sample_processed};
+                2'b01:
+                    sample_fifo <= {sample_processed[3:0], sample_fifo[11:8], sample_processed[7:0]};
+            endcase
+
             byte_vld <= '1;
-        end else if (uart_rdy)
-            byte_vld <= '0;
+            send_rdy <= send_rdy + 2'b01;
+
+        end else if (uart_rdy) begin
+            if (send_rdy==2'b10)
+                send_rdy <= send_rdy + 2'b01;
+            if (send_rdy==2'b11 || send_rdy==2'b00)
+                byte_vld <= 0;
+        end
     end
 end
 
-always_ff @(posedge CLK_50) begin
-    if (RST) begin
-        sample_fifo_rdy <= '0;
-    end else begin
-
-    end
-end
-
-always_ff @(posedge CLK_50) begin
-    if (RST) begin
-        byte_vld <= '0;
-    end else begin
-        if (sample_vld && sample_fifo_rdy) begin
-            byte_vld <= '1;
-        end else if (uart_rdy)
-            byte_vld <= '0;
-    end
+always_comb begin
+    if (send_rdy[1])
+            byte_packed = sample_fifo[15:8];
+    else
+            byte_packed = sample_fifo[7:0]; 
 end
 
 not_my_uart_tx #(
