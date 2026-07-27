1c51eaf63cd9ea7741cf791c4fb7bac9afeaae76
AntonGLi <anthonglinin@yandex.ru>
2026-07-28 00:11:54 +0300

    12b tb uart replaced with bbox for better visual

--- adc_fpga/tb_12_to_8_bit.sv
+++ adc_fpga/tb_12_to_8_bit.sv
@@ -59,14 +59,26 @@ buf_12_to_8 byte_packer (
     .bus_8b_vld_o(byte_vld)
 );
 
-not_my_uart_tx #(
-/*
+logic [7:0] received_value;
+
+receiver #(
+  .NUM_CLK(300)
+) UART (
+  .bus_8b_i(byte_packed),
+  .bus_8b_vld_i(byte_vld),
+  .bus_8b_rdy_o(uart_rdy),
+  .CLK(CLK_50),
+  .RST(RST),
+  .received_value(received_value)
+);
+
+/*not_my_uart_tx #(
+
 NUM_CLK is a half of bit length measured in clock periods.
 60 MHz clk_i
 1-2 MHz bit frequency (1/period)
 1 bit is 30 clk periods
 0.5 bit is 15-16 clk periods
-*/
   .NUM_CLK(30)
 ) UART (
   .clk_i(CLK_50),
@@ -76,4 +88,50 @@ NUM_CLK is a half of bit length measured in clock periods.
   .ready_o(uart_rdy),
   .tx_o(tx_o)
 );
+*/
+endmodule
+
+module receiver #(
+  parameter NUM_CLK=40
+) (
+  input  logic [7:0] bus_8b_i,
+  input  logic bus_8b_vld_i,
+  output logic bus_8b_rdy_o,
+  input  logic CLK,
+  input  logic RST,
+  output logic [7:0] received_value
+);
+
+logic [$clog2(NUM_CLK)-1:0] cnt;
+logic trans;
+assign trans = bus_8b_rdy_o && bus_8b_vld_i;
+
+always @(posedge CLK) begin
+  if (RST || trans || (cnt == NUM_CLK)) 
+    cnt <= '0;
+  else 
+    cnt <= cnt + 1;
+end
+
+always @(posedge CLK) begin
+  if (RST || trans)
+    bus_8b_rdy_o <= '0;
+  else begin
+    if (cnt == NUM_CLK)
+      bus_8b_rdy_o <= '1;
+    else begin
+      if (trans)
+        bus_8b_rdy_o <= '0;
+    end 
+  end
+end
+
+always @(posedge CLK) begin
+  if (RST)
+    received_value <= '0;
+  else begin
+    if (trans)
+      received_value <= bus_8b_i;
+  end
+end
 endmodule
